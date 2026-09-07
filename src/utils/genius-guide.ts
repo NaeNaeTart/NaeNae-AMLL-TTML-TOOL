61f7b3a6fed9e6e632e0902d9ac0d709c514e128
Tman564ws <tman@tman564ws.com>
2026-09-07 13:57:39 -0400

    Changed guide URLs to the new ones

--- src/utils/genius-guide.ts
+++ src/utils/genius-guide.ts
@@ -1,5 +1,3 @@
-export const getGeniusKeyGuideUrl = (language?: string) => {
-	const locale = language?.split("-")[0].toLowerCase();
-	const prefix = locale === "it" || locale === "es" ? `${locale}/` : "";
-	return `https://docs.tx24.dev/${prefix}guides/ttml.html#genius-api-key`;
+export const getGeniusKeyGuideUrl = () => {
+	return "https://guides.spicylyrics.org/s/ttml/doc/2-import-the-lyrics-CK0YxRxPwp#h-create-a-genius-client-access-token";
 };
